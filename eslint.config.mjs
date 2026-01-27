50054d3659f32863f4563d09f361c81708707447
alokgoldy <alokkrgoldy@gmail.com>
2026-01-27 12:30:08 +0530

    feat: added eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [js.configs.recommended, eslintConfigPrettier],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+    },
+  },
+])
