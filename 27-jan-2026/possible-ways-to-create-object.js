9437e43bd710470f8053b4a81c90a05baf4864d3
alokgoldy <alokkrgoldy@gmail.com>
2026-01-28 17:32:02 +0530

    update: added new way to create object

---
+++ 27-jan-2026/possible-ways-to-create-object.js
@@ -0,0 +1,34 @@
+// What are the possible ways to create objects in JavaScript
+
+// 🚀 1. Using object literal notation
+
+const obj = {
+    name: 'alok',
+    role: 'js dev'
+}
+
+// 🚀 2. Using the Object constructor
+
+// The simplest way to create an empty object is using the Object constructor. Currently this approach is not recommended.
+
+const obj2 = new Object();
+obj2.name = 'rahul';
+
+// The Object() is a built-in constructor function so "new" keyword is not required for creating plain objects. The above code snippet can be re-written as:
+
+const obj3 =  Object();
+obj3.name = 'ravi';
+
+
+// 🚀 3. Using the new keyword with a constructor function
+
+function Person(name) {
+    this.name = name;
+    this.age = 21;
+}
+const obj4 = new Person('alok goldy');
+// 🚀 4. Using Object.create()
+
+// 🚀 5. Using class syntax (ES6+)
+
+console.log(obj, obj2, obj3, obj4);
