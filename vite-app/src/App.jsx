12ecdb492fae16b658a33059a8c0b0017bf8fe9a
alokgoldy <alokkrgoldy@gmail.com>
2026-02-17 22:43:16 +0530

    update: added route for infinite - scroll

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,14 +1,13 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import SearchDemo from './pages/SearchDemo'
+import InfiniteScroll from './pages/InfiniteScroll'
 
 function App() {
   return (
     <>
-      <nav style={{ display: 'flex', gap: 12, justifyContent: 'center' }}>
-        <Link to="/">Search Demo</Link>
-      </nav>
       <Routes>
         <Route path="/search-demo" element={<SearchDemo />} />
+        <Route path="/infinite-scroll" element={<InfiniteScroll/>}/>
       </Routes>
     </>
   )
