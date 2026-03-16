f737cc2f5a478d76e75cf927d744a952efbdd134
alokgoldy <alokkrgoldy@gmail.com>
2026-03-16 12:36:45 +0530

    update: event loop

--- questions/event-loop.js
+++ questions/event-loop.js
@@ -38,3 +38,18 @@ console.log("End");
    - setInterval
 
 *******************/
+
+console.log("A")
+
+setTimeout(() => console.log("B"), 0)
+
+Promise.resolve().then(() => {
+  console.log("C")
+  setTimeout(() => console.log("D"), 0)
+})
+
+Promise.resolve().then(() => console.log("E"))
+
+console.log("F")
+
+// A F C E B D
