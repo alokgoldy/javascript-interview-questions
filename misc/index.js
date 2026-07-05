d9749d0eaaeca155c49a255b3c4e3a523331ef73
alokgoldy <alokkrgoldy@gmail.com>
2026-07-05 12:23:09 +0530

    feat:encode decode

--- misc/index.js
+++ misc/index.js
@@ -55,22 +55,22 @@ const obj1 = new Object();
 const obj2 = Object.create(null);
 
 let vehicle = {
-  wheels: "4",
-  fuelType: "Gasoline",
-  color: "Green",
+    wheels: "4",
+    fuelType: "Gasoline",
+    color: "Green",
 };
 let carProps = {
-  type: {
-    value: "Volkswagen",
-  },
-  model: {
-    value: "Golf",
-  },
+    type: {
+        value: "Volkswagen",
+    },
+    model: {
+        value: "Golf",
+    },
 };
 
 var car = Object.create(vehicle, carProps);
 
-function Person(name){
+function Person(name) {
     this.name = name;
     this.age = 21;
 }
@@ -84,4 +84,43 @@ console.log('alok', {
     obj1,
     obj2,
     obj3
+})
+
+const objt = {
+    name: 'alok',
+    roll: 1
+}
+
+function fun(param1, param2) {
+    console.log(`${param1} ${param2} ${this.name}`);
+}
+
+fun.apply(objt, ['Good', 'morning']);
+
+const arr = [1, 2, 3, 4];
+
+const arr2 = arr.splice(2, 1, 'alok', 'goldy');
+
+console.log(arr2, arr);
+
+const mp = new Map();
+mp.set('alok', 2);
+mp.set('goldy', 4);
+
+console.log(mp, {
+    goldy: mp.get('goldy')
+});
+
+const fn1 = (a) => (b) => (c) => console.log(a+b+c);
+fn1(1)(1)(1);
+
+
+let uri = "employeeDetails?name=john&occupation=manager";
+let encoded_uri = encodeURI(uri);
+let decoded_uri = decodeURI(encoded_uri);
+
+console.log({
+    uri,
+    encoded_uri,
+    decoded_uri
 })
